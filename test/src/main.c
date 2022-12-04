27f9723b36021c5a5c6e4f51e8864dae86ae4151
qgymib <qgymib@gmail.com>
2022-12-04 19:35:15 +0800

    add memory leak detect for windows

--- test/src/main.c
+++ test/src/main.c
@@ -1,3 +1,10 @@
+/* Enable memory leak detection for windows. */
+#if defined(_WIN32)
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
 #include <uv.h>
 #include <cutest.h>
 #include <stdlib.h>
@@ -9,6 +16,10 @@ static void _at_exit(void)
 
 int main(int argc, char* argv[])
 {
+#if defined(_WIN32)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
     atexit(_at_exit);
     argv = uv_setup_args(argc, argv);
     return cutest_run_tests(argc, argv, NULL);
